89119fb9cbd083cfb18143c4b92d9a21dd76e0c9
DomWat <domwat20@gmail.com>
2020-10-13 09:30:52 -0500

    changes

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ let marker;
 let addressTextBox = document.getElementById("addressTextBox");
 let searchButton = document.getElementById("searchButton");
 
-<<<<<<< HEAD
-let restroomUL = document.getElementById("restroomUL");
-
-=======
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => console.log(position));
@@ -15,7 +11,6 @@ function getLocation() {
         console.log("Geolocation not supported by user device")
     }
 }
->>>>>>> main
 
 function initMap() {
     // The location of Uluru
@@ -77,59 +72,6 @@ searchButton.addEventListener("click", async function () {
     locationObj.lat,
     locationObj.lng
   );
-<<<<<<< HEAD
-
-
-  //function to initialize map on load
-    let map;
-    let marker;
-
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => console.log(position));
-        } else {
-            console.log("Geolocation not supported by user device")
-        }
-    }
-
-    function initMap() {
-        // The location of Uluru
-        var uluru = { lat: -25.344, lng: 131.036 };
-        // The map, centered at Uluru
-        map = new google.maps.Map(
-            document.getElementById('map'), { zoom: 4, center: uluru });
-        // The marker, positioned at Uluru
-        marker = new google.maps.Marker({ position: uluru, map: map });
-    }
-
-
-    function renderMapAndMarkers(center, markers) {
-        map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 13,
-            center: new google.maps.LatLng(center.lat, center.lng),
-        });
-
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(center.lat, center.lng),
-            icon: 'https://i.pinimg.com/originals/25/62/aa/2562aacd1a4c2af60cce9629b1e05cf2.png',
-            map: map,
-        })
-
-        markers.forEach((marker) => {
-            marker = new google.maps.Marker({
-                position: new google.maps.LatLng(
-                    marker.latitude,
-                    marker.longitude
-                ),
-                map: map,
-            });
-        });
-    }
-
-
-    // displaying all the information on the dispaly UL -Dom
-=======
->>>>>>> main
   renderMapAndMarkers(locationObj, restrooms);
 
   function displayRestroomData() {
