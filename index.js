bc62105f2a777dd8e3d0e5e5d928cf8fc0350e7a
Dom <70596778+DomWat@users.noreply.github.com>
2020-10-14 09:54:57 -0500

    Add filters (#10)
    * got it to replace info with new search
    
    * update
    
    * prepare for merge
    
    * prepare for merge
    
    * update display code
    
    * fix display bug

--- index.js
+++ index.js
@@ -86,17 +86,20 @@ searchButton.addEventListener("click", async function () {
     
     
     console.log(restrooms);
-
-    let work = restrooms.map((restrooms) => {
+    
+// pull information from the API and place on display -Dom
+    let work = restrooms.map((restroom) => {
         return `
         <div id="separate">
-            <li><b>${restrooms.name}</b></li>
-            <li>${restrooms.street} ${restrooms.city}, ${restrooms.state}</li><br>
-            <li>${restrooms.comment}</li>
+            <li><b>${restroom.name}</b></li>
+            <li>${restroom.street} ${restroom.city}, ${restroom.state}</li><br>
+            <li>${restroom.comment}</li>
         </div>
         `
+        
     })
-    restroomUL.insertAdjacentHTML('beforeend', work.join(''))
+    restroomUL.innerHTML = work.join(" ")
+    
 
 
 });
