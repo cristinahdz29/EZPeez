d7f2f7c941d61920499a5008a1ed3ca59ebe0612
Cristina Hernandez <64177857+cristinahdz29@users.noreply.github.com>
2020-10-13 20:54:04 -0400

    removed double function and added missing curly bracket

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ let searchButton = document.getElementById("searchButton");
 let wheelchair = document.getElementById("wheelchair");
 let changing_table = document.getElementById("changing_table");
 let unisex = document.getElementById("unisex");
-let restroomUL = document.getElementById("restroomUL")
+let restroomUL = document.getElementById("restroomUL");
 
 //function to get location for map to show on load
 function getLocation() {
-    //if statement for when page loads, if user's device supports geolocation we will get their coordinates on load
-    if (navigator.geolocation) {
+  //if statement for when page loads, if user's device supports geolocation we will get their coordinates on load
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async function (position) {
       console.log(position);
       //when position is consoled log, it is an object, that has lat and long as cooords
@@ -46,24 +46,24 @@ function initMap() {
 
 //function to get lat and long coordinates by address
 async function getLatAndLogByAddress(address) {
-    let formatedAddress = address.split(" ").join("+");
+  let formatedAddress = address.split(" ").join("+");
 
-    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${formatedAddress}&key=AIzaSyDHy8QmVO1C4nSFZhTo9KZZ24Py0IuHrY4`;
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${formatedAddress}&key=AIzaSyDHy8QmVO1C4nSFZhTo9KZZ24Py0IuHrY4`;
 
-    let response = await fetch(url);
-    let data = await response.json();
+  let response = await fetch(url);
+  let data = await response.json();
 
-    return data.results[0].geometry.location;
+  return data.results[0].geometry.location;
 }
 
 //function get restrooms by lat and long coordinates using the restroom API
 async function getRestroomsByLatAndLog(lat, lng) {
-    let url = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&lat=${lat}&lng=${lng}`;
+  let url = `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&lat=${lat}&lng=${lng}`;
 
-    let response = await fetch(url);
-    let data = await response.json();
+  let response = await fetch(url);
+  let data = await response.json();
 
-    return data;
+  return data;
 }
 
 function renderMapAndMarkers(center, markers) {
@@ -87,90 +87,69 @@ function renderMapAndMarkers(center, markers) {
   });
 
   markers.forEach((marker) => {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 13,
-        center: new google.maps.LatLng(center.lat, center.lng),
+    new google.maps.Marker({
+      position: new google.maps.LatLng(marker.latitude, marker.longitude),
+      map: map,
     });
-
-    marker = new google.maps.Marker({
-        position: new google.maps.LatLng(center.lat, center.lng),
-        icon:
-            "https://i.pinimg.com/originals/25/62/aa/2562aacd1a4c2af60cce9629b1e05cf2.png",
-        map: map,
-    });
-
-    markers.forEach((marker) => {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(marker.latitude, marker.longitude),
-            map: map,
-        });
-    });
-  }
+  });
+}
 
 //Adding event listener to search button
 searchButton.addEventListener("click", async function () {
-    
+  locationObj = await getLatAndLogByAddress(addressTextBox.value);
+  restrooms = await getRestroomsByLatAndLog(locationObj.lat, locationObj.lng);
+  renderMapAndMarkers(locationObj, restrooms);
 
-    locationObj = await getLatAndLogByAddress(addressTextBox.value);
-    restrooms = await getRestroomsByLatAndLog(
-        locationObj.lat,
-        locationObj.lng);
-    renderMapAndMarkers(locationObj, restrooms);
+  console.log(restrooms);
 
-    
-    
-    console.log(restrooms);
-
-    let work = restrooms.map((restrooms) => {
-        return `
+  let work = restrooms.map((restrooms) => {
+    return `
         <li><h2><b>${restrooms.name}</b><h2><br>
             <ul>
                 <li>${restrooms.street} ${restrooms.city}, ${restrooms.state}</li>
                 <li>${restrooms.comment}</li>
             </ul>
         </li>
-        `
-    })
-    restroomUL.insertAdjacentHTML('beforeend', work.join(''))
-
-
+        `;
+  });
+  restroomUL.insertAdjacentHTML("beforeend", work.join(""));
 });
 
-wheelchair.addEventListener("click", async function() {
+wheelchair.addEventListener("click", async function () {
+  if (this.checked) {
+    let response = await fetch(
+      `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&lat=${locationObj.lat}&lng=${locationObj.lng}`
+    );
+    let json = await response.json();
+    let wheelchairItems = json.filter((restroom) => {
+      return restroom.accessible == true;
+    });
+    console.log(wheelchairItems);
+  }
+});
 
-    if(this.checked) {
-        let response = await fetch(`https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&lat=${locationObj.lat}&lng=${locationObj.lng}`)
-        let json = await response.json()
-        let wheelchairItems = json.filter((restroom) => {
-            return restroom.accessible == true        
-})
-        console.log(wheelchairItems)
+changing_table.addEventListener("click", async function () {
+  if (this.checked) {
+    let response = await fetch(
+      `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&lat=${locationObj.lat}&lng=${locationObj.lng}`
+    );
+    let json = await response.json();
+    let changing_tableItems = json.filter((restroom) => {
+      return restroom.changing_table == true;
+    });
+    console.log(changing_tableItems);
+  }
+});
 
-}
-})
-
-changing_table.addEventListener("click", async function() {
-
-    if(this.checked) {
-        let response = await fetch(`https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&lat=${locationObj.lat}&lng=${locationObj.lng}`)
-        let json = await response.json()
-        let changing_tableItems = json.filter((restroom) => {
-            return restroom.changing_table == true        
-})
-        console.log(changing_tableItems)
-
-}
-})
-
-unisex.addEventListener("click", async function() {
-
-    if(this.checked) {
-        let response = await fetch(`https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&lat=${locationObj.lat}&lng=${locationObj.lng}`)
-        let json = await response.json()
-        let unisexItems = json.filter((restroom) => {
-            return restroom.unisex == true        
-})
-        console.log(unisexItems)
-
-}
-})
+unisex.addEventListener("click", async function () {
+  if (this.checked) {
+    let response = await fetch(
+      `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&lat=${locationObj.lat}&lng=${locationObj.lng}`
+    );
+    let json = await response.json();
+    let unisexItems = json.filter((restroom) => {
+      return restroom.unisex == true;
+    });
+    console.log(unisexItems);
+  }
+});
