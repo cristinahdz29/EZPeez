ce24c3187363c0023bf3bc26c21ec3d61008d627
Cristina Hernandez <64177857+cristinahdz29@users.noreply.github.com>
2020-10-15 21:06:01 -0400

    direction button modifications

--- index.js
+++ index.js
@@ -179,12 +179,13 @@ searchButton.addEventListener("click", async function () {
             }</li> 
             </div>
 
-            <div id="searchButton">
+            <div id="directionsButton">
             <a href="https://www.google.com/maps/dir/?api=1&destination=${
               restroom.latitude
             },${
           restroom.longitude
-        }" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M21.71 11.29l-9-9c-.39-.39-1.02-.39-1.41 0l-9 9c-.39.39-.39 1.02 0 1.41l9 9c.39.39 1.02.39 1.41 0l9-9c.39-.38.39-1.01 0-1.41zM14 14.5V12h-4v2c0 .55-.45 1-1 1s-1-.45-1-1v-3c0-.55.45-1 1-1h5V7.5l3.15 3.15c.2.2.2.51 0 .71L14 14.5z"/></svg><a>
+        }" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M21.71 11.29l-9-9c-.39-.39-1.02-.39-1.41 0l-9 9c-.39.39-.39 1.02 0 1.41l9 9c.39.39 1.02.39 1.41 0l9-9c.39-.38.39-1.01 0-1.41zM14 14.5V12h-4v2c0 .55-.45 1-1 1s-1-.45-1-1v-3c0-.55.45-1 1-1h5V7.5l3.15 3.15c.2.2.2.51 0 .71L14 14.5z"/></svg></a>
+        </div>
         </div>
         </div>
         `;
